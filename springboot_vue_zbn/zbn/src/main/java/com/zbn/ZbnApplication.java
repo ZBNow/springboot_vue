e980667d35f2bb6108966e134cc2e5433fb2669f
ZBNow <14922268939@qq.com>
2025-03-21 10:28:36 +0800

    delete target

--- springboot_vue_zbn/zbn/src/main/java/com/zbn/ZbnApplication.java
+++ springboot_vue_zbn/zbn/src/main/java/com/zbn/ZbnApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.zbn.mapper")//扫描mapper接口层
 public class ZbnApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ZbnApplication.class, args);
     }
-
 }
